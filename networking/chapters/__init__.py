8bd1f385125e9edfdef7e1cd67f07f7b2f38ac1b
Nikita Sychev <root@nsychev.ru>
2025-06-18 14:56:18 +0500

    Add practice chapter

--- networking/chapters/__init__.py
+++ networking/chapters/__init__.py
@@ -3,8 +3,9 @@ from networking.chapters.dhcpd import DHCPDChapter
 from networking.chapters.dns import DNSChapter
 from networking.chapters.firewall import FirewallChapter
 from networking.chapters.ip import IPChapter
+from networking.chapters.practice import PracticeChapter
 from networking.core.chapter.base import BaseChapter
 
-chapters: list[BaseChapter] = [IPChapter(), DHCPChapter(), DHCPDChapter(), DNSChapter(), FirewallChapter()]
+chapters: list[BaseChapter] = [IPChapter(), DHCPChapter(), DHCPDChapter(), DNSChapter(), FirewallChapter(), PracticeChapter()]
 
 __all__ = ["chapters"]
---
+++ networking/chapters/practice.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+from decimal import Decimal
+
+from starlette.requests import Request
+
+from networking.core import util
+from networking.core.chapter.base import BaseChapter, ChapterTask
+
+
+class PracticeVariant:
+    ...
+
+
+class PracticeChapter(BaseChapter[PracticeVariant]):
+    slug = "practice"
+    name = "Обжим витой пары"
+    deadline = datetime(2025, 5, 24, 21, 0, 0)
+    tasks = [
+        ChapterTask("practice", "Практическое задание", Decimal(10))
+    ]
+
+    @util.scope_cached("variant")
+    async def get_variant(self, request: Request) -> PracticeVariant:
+        return PracticeVariant()
+
+
+__all__ = ["PracticeChapter"]
