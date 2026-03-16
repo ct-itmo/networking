3b44c8a0376acd02d176b065776017da16606e86
Nikita Sychev <root@nsychev.ru>
2026-03-16 15:13:09 +0500

    Support reaping inactive instances

--- build-images.sh
+++ build-images.sh
@@ -4,6 +4,7 @@ set -e
 
 BASE="$(dirname "$(realpath "$BASH_SOURCE")")"
 
+docker build -t ct-itmo/labs-net-helper $BASE/quirck/quirck/box/helper
 docker build -t ct-itmo/quirck-relay $BASE/quirck/quirck/box/relay
 docker build -t ct-itmo/labs-networking-ping $BASE/networking/images/ping
 docker build -t ct-itmo/labs-networking-dhcp-dnsmasq $BASE/networking/images/dhcp-dnsmasq
--- networking/core/chapter/docker.py
+++ networking/core/chapter/docker.py
@@ -8,7 +8,7 @@ from starlette.responses import RedirectResponse, Response
 from starlette.routing import Route
 
 from quirck.auth.model import User
-from quirck.box.docker import launch, lock_meta
+from quirck.box.docker import launch, lock_meta, stop
 from quirck.box.exception import DockerConflict
 from quirck.box.meta import Deployment
 
@@ -23,7 +23,8 @@ class DockerMixin(BaseChapter[DockerTaskProtocol]):
     def __init__(self):
         super().__init__()
         self.routes += [
-            Route("/launch", self.launch, name="launch", methods=["POST"])
+            Route("/launch", self.launch, name="launch", methods=["POST"]),
+            Route("/stop", self.stop, name="stop", methods=["POST"]),
         ]
     
     async def launch(self, request: Request) -> Response:
@@ -40,6 +41,28 @@ class DockerMixin(BaseChapter[DockerTaskProtocol]):
             task = BackgroundTask(launch, session, meta, variant.deployment)
         except DockerConflict:
             task = None
+
+        return RedirectResponse(
+            request.url_for(f"networking:{self.slug}:page"), status_code=303,
+            background=task
+        )
+
+    async def stop(self, request: Request) -> Response:
+        session: AsyncSession = request.scope["db"]
+        user: User = request.scope["user"]
+
+        if self.private and not user.is_admin:
+            raise HTTPException(403, "Доступ запрещён")
+    
+        try:
+            meta = await lock_meta(session, user.id, self.slug)
+            task = BackgroundTask(stop, session, user.id)
+        except DockerConflict:
+            task = None
+
+        meta = await lock_meta(session, user.id, self.slug)
+        if meta.state == "READY":
+            await meta.stop()
 
         return RedirectResponse(
             request.url_for(f"networking:{self.slug}:page"), status_code=303,
